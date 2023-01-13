31a75b31eec037483c46db78c0b437818c9750c5
Melanie R <102996216+melanietr98@users.noreply.github.com>
2023-01-13 15:27:11 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,62 @@
+//Define a function, as a function declaration, maxOfTwoNumbersthat takes two numbers as arguments and returns the largest of them. If they are the same, return that number. Use the if-else construct or a ternary expression - the Math.max method is not allowed.
+
+//function maxOfTwoNumbers(num1, num2){
+  
+ //   if (num1 > num2) {
+  //     console.log(num1 + " is larger than " + num2);
+ //   } else {
+ //       console.log(num1 + " is less than " + num2);
+ //   }
+//    return;
+  
+//}
+
+//maxOfTwoNumbers(12,24);
+
+// Define a function, as a function declaration, isCharAVowelthat takes a character as an argument and returns true if it is a vowel, false otherwise.
+
+//function isCharAVowel(char) {
+ // if (char == "a" || char == "e" || char == "i" || char == "o" || char == "u") {
+//return true;
+//  } else {
+//    return false;
+//  }
+//}
+//console.log(isCharAVowel("k"));
+
+//Define a function, as a function expression, maxOfThreethat takes three numbers as arguments and returns the largest of them. Again, the Math.max method is not allowed.
+
+//const maxOfThreeThat = function(num1, num2, num3) {
+ //   if((num1 >= num2) && (num1 >= num3)) {
+//      return num1;
+//    } else if ((num2 >= num1) && (num2 >= num3)) {
+//    return num2;
+//  } else {
+//    return num3;
+//  }
+//}
+//console.log(maxOfThreeThat(3, 2, 0))
+
+//Define a function, as a function expression, sumArraythat takes an array of numbers and returns the sum of those numbers. For example, sumArray([2, 4, 5]);would return 11.
+
+// const sumArray = function(nums) {
+ //   let sum = 0;
+ 
+  //  for (let a = 0; a < nums.length; a++) {
+ //       sum += nums[a]
+//    }
+// 
+//    return sum;
+//}
+//console.log(sumArray([2, 4, 5]));
+
+//Define a function, as a function declaration, multiplyArraythat takes an array of numbers and returns the product those numbers. For example, multiplyArray([2, 4, 5]);would return 40.
+
+//function multiplyArray(num) {
+//    let product =1;
+ //   for (let i=0; i < num.length; i++) {
+  //      product *= num[i];
+ //   }
+//    return product;
+//}
+//console.log("the answer is " + multiplyArray([2, 4, 5]));
