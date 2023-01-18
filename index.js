3fc5ee134889bde6d8e20774d13733234ee0edd1
Melanietr <>
2023-01-18 03:18:47 +0000

    first commit

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 //Define a function, as a function declaration, maxOfTwoNumbersthat takes two numbers as arguments and returns the largest of them. If they are the same, return that number. Use the if-else construct or a ternary expression - the Math.max method is not allowed.
 
 //function maxOfTwoNumbers(num1, num2){
